8265f9819fe024284ea5089bf0101f9f31bc9978
Vadim <unistars@mail.ru>
2020-11-26 20:02:45 +0300

    Updates

---
+++ precache-manifest.2b62edb79cddaac5d1c5ee8d949a0832.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a168a7f93dd2e5abcade65d89e2b361a",
+    "url": "/socialNetwork/index.html"
+  },
+  {
+    "revision": "df6aa46b3a326a6ff2b9",
+    "url": "/socialNetwork/static/css/main.e55f5cdb.chunk.css"
+  },
+  {
+    "revision": "43737b39713031729a6f",
+    "url": "/socialNetwork/static/js/2.70024734.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/socialNetwork/static/js/2.70024734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6aa46b3a326a6ff2b9",
+    "url": "/socialNetwork/static/js/main.82a42a4f.chunk.js"
+  },
+  {
+    "revision": "ed6928aa620c67eb2582",
+    "url": "/socialNetwork/static/js/runtime-main.a9221900.js"
+  },
+  {
+    "revision": "413fae214a02e48244d8814c17bc2200",
+    "url": "/socialNetwork/static/media/Spinner.413fae21.svg"
+  },
+  {
+    "revision": "0d865fef2251d6e2b45605153bb05d07",
+    "url": "/socialNetwork/static/media/user.0d865fef.png"
+  }
+]);
